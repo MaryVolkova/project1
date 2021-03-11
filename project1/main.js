c7079a24aec0db4bcd2a602d8305fa9c546ed286
Mary Volkova <mashavolkova816@gmail.com>
2021-03-11 08:46:01 +0200

    Add project1

---
+++ project1/main.js
@@ -0,0 +1,5 @@
+const { min, abs, summ } = require('math')
+
+console.log(`min -  ${min([4, 2, 10, 7, -64, 0, -5, 9])}`)
+console.log(`abs -  ${abs(-24)}`)
+console.log(`summ -  ${summ(10, 3, 14, 67, 100)}`)
